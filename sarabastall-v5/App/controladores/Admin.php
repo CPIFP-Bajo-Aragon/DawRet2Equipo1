7f84fc6cb752056db3de2a8dcd802fe2931368c8
victor <victor11val@gmail.com>
2023-01-31 09:48:54 +0100

    Sarabastallv5 y sql

---
+++ sarabastall-v5/App/controladores/Admin.php
@@ -0,0 +1,74 @@
+<?php
+    class Admin extends Controlador{
+
+        public function __construct(){
+
+            Sesion::iniciarSesion($this->datos);
+
+            //$this->datos["usuarioSesion"] = $this->asesoriaModelo->getPersona(1);
+            $this->cursoModelo = $this->modelo('CursoModelo');
+            $this->centroModelo = $this->modelo('CentroModelo');
+            $this->prestamoModelo = $this->modelo('PrestamoModelo');
+            $this->becaModelo = $this->modelo('BecaModelo');
+            $this->economiaModelo = $this->modelo('EconomiaModelo');
+            $this->personaModelo = $this->modelo('PersonaModelo');
+
+            $this->datos["rolesPermitidos"] = [1];
+
+            if(!tienePrivilegios($this->datos["usuarioSesion"]->Id_Rol, $this->datos["rolesPermitidos"])){
+                exit();
+            }
+
+        }
+        
+        public function index(){
+
+            $this->vista("menu",$this->datos);
+
+        }
+
+        public function gestionar_centros(){
+
+            $this->datos["centros"] = $this->centroModelo->get_centros();
+
+            $this->vista("gestion/centros",$this->datos);
+        }
+
+        public function gestionar_prestamos(){
+
+            $this->datos["PrestamosTotales"] = $this->prestamosModelo->get_prestamos();
+            
+            $this->vista("gestion/prestamos",$this->datos);
+        }
+
+        public function gestionar_cursos(){
+            
+            $this->datos["CursosTotales"] = $this->cursoModelo->get_cursos();
+
+            $this->vista("gestion/cursos",$this->datos);
+        }
+
+        public function gestionar_personas(){
+
+            $this->datos["PersonasTotales"] = $this->personaModelo->get_personas();
+            
+            $this->vista("gestion/personas",$this->datos);
+        }
+
+        public function gestionar_becas(){
+
+            $this->datos["BecasTotales"] = $this->becaModelo->get_becas();
+            
+            $this->vista("gestion/becas",$this->datos);
+        }
+
+        public function gestionar_economia(){
+
+            $this->datos["MovimientosTotales"] = $this->economiaModelo->get_movimientos();
+            
+            $this->vista("gestion/economia",$this->datos);
+        }
+
+    }
+
+?>
---
+++ sarabastall-v5/App/modelos/BecaModelo.php
@@ -0,0 +1,13 @@
+<?php
+    class BecaModelo{
+        private $db;
+
+        public function __construct(){
+            $this->db = new Base;
+        }
+
+        //Funciones de Beca
+
+    }
+
+?>
