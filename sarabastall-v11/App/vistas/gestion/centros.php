2efcf6c674890ffe7d02c8ae48006223047e4482
albalorenzm <albalorenzmata03@gmail.com>
2023-02-08 10:32:28 +0100

    mvc 11 -a

---
+++ sarabastall-v11/App/vistas/gestion/centros.php
@@ -0,0 +1,133 @@
+<?php require_once RUTA_APP.'/vistas/inc/header.php'?>
+
+<div class="container">
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo RUTA_URL ?>/admin/index">Menu</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Gestion Centros</li>
+    </ol>
+</nav>
+    <h1>CENTROS</h1>
+
+
+<!-- Ventana modal para insertar un nuevo centro -->
+<div id="ventanaModal" class="modalJS">
+    <div class="contenido-modal">
+        <span class="cerrar">&times;</span>
+        <h4 class="tituloModal">Crear nuevo centro</h4>
+        <br>
+        <hr>
+        <form method="post" action="<?php echo RUTA_URL ?>/admin/add_centro">
+            <label>Nombre Centro:</label>
+            <input type="text" id="nombreCentro" name="nombreCentro">
+            <p id="ErrorNombre"></p>
+
+            <label>Ciudad:</label>
+            <select name="selectNombre">
+              <?php foreach($datos["ciudades"] as $ciudad): ?>
+                <option value="<?php echo $ciudad->Id_Ciudad ?>"><?php echo $ciudad->Nombre_Ciudad?></option>
+              <?php endforeach ?>
+            </select>
+
+            <label>Cuantia:</label>
+            <input type="number" step="any" id="cuantia" name="cuantia"> 
+            <p id="ErrorCuantia"></p>
+
+            <input type="submit" value="Insertar" onclick="todos()">
+        </form>
+
+    </div>
+</div>
+
+<!-- Ventana modal para añadir nuevas ciudades -->
+<div id="MODAlciudad" class="modalJS">
+    <div class="contenidoModal">
+        <span class="cerrarVentana">&times;</span>
+        <h4 class="titulo">Crear nuevas ciudades</h4>
+        <br>
+        <br>
+        <form method="post" action="<?php echo RUTA_URL ?>/admin/add_ciudad">
+            <label>Nombre Ciudad:</label>
+            <input type="text" id="Nombre_Ciudad" name="Nombre_Ciudad">
+
+            <label>Distancia:</label>
+            <input type="number" step="any" id="Distancia" name="Distancia">
+
+            <input type="submit" value="Agregar" onclick="todos()">
+        </form>
+    </div>
+</div>
+
+
+<!-- Modal para  confirmar que elimina un registro de la tabla-->
+<div class="modal fade" id="modalEliminarCentro" tabindex="-1">
+  <div class="modal-dialog modal-dialog-center"> 
+      <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title" id="modalCerrarAccionLabel">
+                  ¿Seguro qué desea eliminar el centro?
+              </h5>
+          </div>
+
+          <div  class="modal-footer"> 
+              <form method="post" id="formCerrarAccion" action="<?php echo RUTA_URL ?>/admin/del_centro">
+                <input type="hidden" id="Id_Eliminar" name="Id_Centro">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-warning">Eliminar</button>
+              </form>
+          </div>
+      </div>
+  </div> 
+</div>
+
+<!-- Contenedor principal con la tabla -->
+<div id="container">
+<button id="botonCiudad">Añadir Nueva Ciudad</button>
+
+
+    <button id="abrirModal">+</button>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+      <br>
+        <table class="table table-striped table-hover">
+          <thead class="thead-azul">
+            <tr>
+                <th scope="col">Nº Centro</th>
+                <th scope="col">Nombre Centro</th>
+                <th scope="col">Ciudad</th>
+                <th scope="col">Cuantia</th>
+                <th scope="col">Distancia</th>
+                <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <?php foreach ($datos["centros"] as $centro):  ?>
+
+                    <th scope="row"><?php echo $centro ->Id_Centro?></th>
+                    <td><?php echo $centro->Nombre?></td>
+                    <td><?php echo $centro->Nombre_Ciudad?></td> 
+                    <td><?php echo $centro->Cuantia?></td>
+                    <td><?php echo $centro->Distancia?></td>
+
+                    <td>
+                      <a  href="<?php echo RUTA_URL ?>/admin/verCentro/<?php echo $centro->Id_Centro ?>">
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#VerMas" class="w-80 btn btn-warning btn-lg">
+                          <i class="bi bi-pencil-square"></i>
+                        </button>
+                      </a>
+                      <button type="button" onclick="place_id(<?php echo $centro->Id_Centro ?>)" data-bs-toggle="modal" data-bs-target="#modalEliminarCentro" class="w-80 btn btn-warning btn-lg">
+                          <i class="bi bi-trash"></i>
+                      </button>
+                    </td>
+            </tr>
+            <?php endforeach?>
+          </tbody>
+        </table>
+                </div>
+</div>
+
+    
+<?php require_once RUTA_APP.'/vistas/inc/footer.php'?>
---
+++ sarabastall-v11/App/vistas/gestion/economia.php
@@ -0,0 +1,144 @@
+<?php require_once RUTA_APP.'/vistas/inc/header.php'?>
+
+<div class="container">
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo RUTA_URL ?>/admin/index">Menu</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Gestion Economia</li>
+    </ol>
+</nav>
+
+    <h1>ECONOMIA</h1>
+
+    <!-- <div id="ModalMovimientos" class="modalJS"> -->
+        <div class="content" id="ModalMovimientos">
+            <span class="salir">&times;</span>
+            <h4 class="title">Añadir nuevos movimientos</h4>
+            <br>
+            <hr>
+            <form method="post" action="<?php echo RUTA_URL?>/admin/add_movimiento">
+                <label>Concepto Movimiento:</label>
+                <input type="text" id="concepto" name="concepto">
+                <p id="ErrorNombre"></p>
+
+                <label>Fecha:</label>
+                <input type="date" id="fecha" name="fecha">
+                <p id="ErrorFecha"></p>
+
+                <label>Cantidad:</label>
+                <input type="number" step="any" id="cuantia" name="cuantia"> 
+                <p id="ErrorCuantia"></p>
+
+                <label>Tipo Movimiento:</label>
+                <select name="selectMovimiento">
+                <?php foreach($datos["tipoMov"] as $tipoMovimiento): ?>
+                    <option value="<?php echo $tipoMovimiento->Id_TipoMov ?>"><?php echo $tipoMovimiento->Nombre_TipoMov?></option>
+                <?php endforeach ?>
+                </select>
+
+                <label>Beca relacionada</label>
+                <select name="selectBeca">
+                    <option value="" cheked></option>
+                    <?php foreach($datos['tipoBeca'] as $tipoBeca): ?>
+                        <option value="<?php echo $tipoBeca->Id_Beca ?>"><?php echo "Id Beca: " ?><?php echo $tipoBeca->Id_Beca ?><?php echo " - " ?><?php echo "Importe: "?><?php echo $tipoBeca->Importe ?></option>
+                    <?php endforeach ?>
+                </select>
+                <p>***Si el movimiento que desea hacer está relacionado a una Beca, por favor inserte el id de la beca, si no, dejelo sin rellenar.</p>
+
+                <input type="submit" value="Añadir nuevo movimiento" onclick="todos()">
+            </form> 
+        </div>
+    <!-- </div>  -->
+
+
+<div id="contenido">
+    <br>
+    <br>
+        <button id="insert">+</button>
+
+        <br>
+
+        <!-- <form method="post" action="<?php echo RUTA_URL ?>/admin/add_movimiento">
+                <label>Concepto Movimiento:</label>
+                <input type="text" id="concepto" name="concepto">
+                <p id="ErrorNombre"></p>
+
+                <label>Fecha:</label>
+                <input type="date" id="fecha" name="fecha">
+                <p id="ErrorFecha"></p>
+
+                <label>Cantidad:</label>
+                <input type="number" step="any" id="cuantia" name="cuantia"> 
+                <p id="ErrorCuantia"></p>
+
+                <label>Tipo Movimiento:</label>
+                <select name="selectMovimiento">
+                <?php foreach($datos["tipoMov"] as $tipoMovimiento): ?>
+                    <option value="<?php echo $tipoMovimiento->Id_TipoMov ?>"><?php echo $tipoMovimiento->Nombre_TipoMov?></option>
+                <?php endforeach ?>
+                </select>
+
+                <label>Beca relacionada</label>
+                <select name="selectBeca">
+                    <option value="" cheked></option>
+                    <?php foreach($datos['tipoBeca'] as $tipoBeca): ?>
+                        <option value="<?php echo $tipoBeca->Id_Beca ?>"><?php echo "Id Beca: " ?><?php echo $tipoBeca->Id_Beca ?><?php echo " - " ?><?php echo "Importe: "?><?php echo $tipoBeca->Importe ?></option>
+                    <?php endforeach ?>
+                </select>
+                <p>***Si el movimiento que desea hacer está relacionado a una Beca, por favor inserte el id de la beca, si no, dejelo sin rellenar.</p>
+
+                <input type="submit" value="Añadir nuevo movimiento" onclick="todos()">
+            </form> -->
+
+
+
+
+
+        <br>
+        <br>
+            <table class="table table-striped table-hover">
+                <thead class="thead-azul">
+                    <tr>
+                        <th scope="col">Nº Movimiento</th>
+                        <th scope="col">Concepto</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Tipo Movimiento</th>
+                        <th scope="col">Id Beca</th>
+                        <!-- <th scope="col">Acciones</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach($datos["movimientos"] as $movimiento):?>
+                    <tr>
+                        
+                            <th scope="row"><?php echo $movimiento->Id_Movimiento?></th>
+                            <td><?php echo $movimiento->Procedencia?></td>
+                            <td> <?php echo formatFecha($movimiento->Fecha)?></td>
+                            <td><?php echo $movimiento->Cantidad?></td>
+                            <td><?php echo $movimiento->Nombre_TipoMov?></td>
+                            <td><?php echo $movimiento->Id_Beca?></td>
+
+
+                            <!-- <td>
+                            href="<?php echo RUTA_URL ?>/admin/verCentro/<?php echo $centro->Id_Centro ?>" -->
+                                <!-- <a>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#VerMas" class="w-80 btn btn-warning btn-lg">
+                                    <i class="bi bi-pencil-square"></i>
+                                    </button>
+                                </a> 
+                            </td> -->
+                           
+                    </tr>
+                    <?php endforeach?>
+                </tbody>
+            </table>
+    </div>
+
+
+
+
+
+
+
+<?php require_once RUTA_APP.'/vistas/inc/footer.php'?>
